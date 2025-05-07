d1a5ca2734587eb4c57f2e79f596fbbc67e59879
Heloiisee <Heloiseguicheron71@gmail.com>
2025-05-06 22:24:33 -0400

    Modification

--- app/Http/Controllers/Admin/CertificationController.php
+++ app/Http/Controllers/Admin/CertificationController.php
@@ -102,7 +102,7 @@ class CertificationController extends Controller
                 Storage::disk('public')->delete($certification->pdf);
             }
 
-            $certification->pdf = $request->file('pdf')->store('storage/pdfs', 'public');
+            $certification->pdf = $request->file('pdf')->store('pdfs', 'public');
         }
 
         $certification->nom = $request->nom;
