e487c16df93670bbdfdc0b41e628157deec6f55e
Heloiisee <Heloiseguicheron71@gmail.com>
2025-05-19 23:22:20 -0400

    Modification du footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container reveal">
         <div class="row">
             <div class="col-md-6 mb-4 mb-md-0 reveal-1">
-                <h5 class="fw-bold">Réseaux sociaux</h5>
+                <h5 class="fw-bold text-light">Réseaux sociaux</h5>
                 <ul class="list-unstyled">
                     <li>
                         <a href="https://www.linkedin.com/in/melissa-guicheron-6a6201290/" 
@@ -16,7 +16,7 @@
             </div>
 
             <div class="col-md-6 reveal-2">
-                <h5 class="fw-bold">Navigation</h5>
+                <h5 class="fw-bold text-light">Navigation</h5>
                 <ul class="list-unstyled">
                     <li><a href="{{ url('/') }}" class="text-light text-decoration-none">Accueil</a></li>
                     <li><a href="{{ route('projects.index') }}" class="text-light text-decoration-none">Projets</a></li>
@@ -29,7 +29,7 @@
         </div>
 
         <div class="text-center mt-4 pt-3 border-top border-light-subtle">
-            <small class="text-secondary">
+            <small class="text-light">
                 &copy; {{ now()->year }} Melissa Guicheron. Tous droits réservés.
             </small>
         </div>
